3878e8d4542d0f0b3eb812951f94cd596a25f528
arati nankar <arati.nankar@plantria.com>
2015-10-05 13:46:56 +0530

    validation

--- app/scripts/controllers/change-password.js
+++ app/scripts/controllers/change-password.js
@@ -9,12 +9,29 @@
  */
 angular.module('clarionEnterpriseApp')
   .controller('ChangePasswordCtrl', function ($scope,authFactory,$location) {
+
+      $scope.vm = {
+        email : '',
+        message: "",
+        flag: false,
+        successFlag:false
+     };
+    
   	 $scope.changepassword = function () {
       // make API call only if the form is valid
     var userData =JSON.parse(sessionStorage.getItem('userData'));    
     authFactory.changePassword({email:userData.data['email'], password: $scope.cp.currentpassword, newPassword: $scope.cp.password }).then(function(response){
-    		alert("password change sucessfully");
-  			console.log(response);
+    		 if(response.success) {
+           $scope.vm.flag= false;
+           $scope.vm.successFlag=true;
+           $scope.vm.message = response.data.message;
+          
+        } else {
+            $scope.vm.flag= true;
+            $scope.vm.successFlag=false;
+            $scope.vm.message = response.message;
+        }
+        console.log(response);
   		});
     };
   });
